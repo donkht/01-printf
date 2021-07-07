fe5aac07118046eb7522cce59dc5bd4cf516cb11
donkht <ggodot@gmail.com>
2021-07-07 05:20:03 +0300

    Add files via upload

---
+++ printf.c
@@ -0,0 +1,43 @@
+#include "printf.h"
+#include <stdio.h>
+
+int ft_printf(const char *s, ...)
+{
+	va_list ap;
+
+	va_start(ap, s);
+
+
+
+
+	write(1, s, ft_strlen(s));
+	return 0;
+}
+
+int	ft_strlen(const char *s)
+{
+	int	len;
+
+	len = 0;
+	while (*(s + len) != 0)
+		len++;
+	return (len);
+}
+
+int	main(void)
+{
+	int count;
+//	int i = 0xffffff;
+	// char *c = "1234567890";
+
+	count = printf("s");
+	printf("%i\n", count);
+
+	count = ft_printf("s");
+	printf("%i\n", count);
+
+	return(0);
+}
+
+
+
---
+++ printf.h
@@ -0,0 +1,6 @@
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+int ft_printf(const char *s, ...);
+int	ft_strlen(const char *s);
