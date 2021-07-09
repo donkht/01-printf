27aa2f3181b37fa7a796ed863f4a0d94fc4cbdb9
Neelix Leyton <nleyton@oa-o3.msk.21-school.ru>
2021-07-09 09:26:23 +0300

    somthing new

--- printf.c
+++ printf.c
@@ -12,17 +12,11 @@ int ft_printf(const char *s, ...)
 	{
 		if (*s != '%')
 		{
-			write(1, s, 1);
+			write(1, s++, 1);
 			count++;
 		}
-		if (*s == '%')
-		{
-
-
-
-
-		}
-		s++;
+		if (*s++ == '%')
+			count = count + ft_process(&s, ap);
 	}
 
 
@@ -30,6 +24,56 @@ int ft_printf(const char *s, ...)
 
 	va_end(ap);
 	return count;
+}
+
+int	ft_process(char **s, va_list ap)
+{
+	flags	f;
+
+	f = ft_init_flags(f);
+	while (**s != 'c' && != **s != 's' && **s != 'i' && )**s != 'd' && **s != 'u' && **s != 'x' && **s != 'X' && **s != 'p')
+	{
+		if (**s == '-')
+			f.minus = 1;
+		if (**s == '0')
+			f.zero = 1;
+		if (**s == ' ')
+			f.space = 1;
+		if (**s == '#')
+			f.hash = 1;
+		if (**s == '+')
+			f.plus = 1;
+		if (**s == '')
+			f.width = 1;
+		if (**s == '')
+			f.precision = 1;
+		if (**s == '')
+			f.type = 1;
+
+
+
+
+	}
+
+
+
+
+
+
+
+}
+
+flags	ft_init_flags(flags f)
+{
+	f.minus = 0;
+	f.zero = 0;
+	f.width = 0;
+	f.precision = 0;
+	f.hash = 0;
+	f.space = 0;
+	f.plus = 0;
+	f.type = 0;
+	return (f);
 }
 
 int	ft_strlen(const char *s)
@@ -48,11 +92,11 @@ int	main(void)
 //	int i = 0xffffff;
 	// char *c = "1234567890";
 
-	count = printf("1234567890qwertyuiop");
-	printf("_%i\n", count);
+	count = printf("1234567890qwertyuiop\n");
+	printf("_%0.0i_\n", count);
 
-	count = ft_printf("1234567890qwertyuiop");
-	printf("_%i\n", count);
+	count = ft_printf("1234567890qwertyuiop\n");
+	printf("_%05i_\n", count);
 
 	return(0);
 }
--- printf.h
+++ printf.h
@@ -1,6 +1,28 @@
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdarg.h>
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+
+typedef struct s_flags{
+	int		minus;
+	int		zero;
+	int		width;
+	int		precision;
+	int		hash;
+	int		space;
+	int		plus;
+	char	type;
+} flags;
 
 int ft_printf(const char *s, ...);
 int	ft_strlen(const char *s);
+int	ft_process(char **s, va_list ap);
+flags	ft_init_flags(flags f);
+
+#endif
+
+//Manage any combination of the following flags: ’-0.’ and minimum field width
+//with all conversions
+//• Manage all the following flags: ’# +’ (yes, one of them is a space)
